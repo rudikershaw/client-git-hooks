7fdac79389ffcdbb556a6203a8afb78a7d5e8321
Rudi Kershaw <rudi.kershaw@cognitran.com>
2019-09-29 15:31:16 +0100

    Add configurable max file size.

--- git-hooks/pre-commit-hooks/maximum-file-size.sh
+++ git-hooks/pre-commit-hooks/maximum-file-size.sh
@@ -2,9 +2,9 @@
 set -e
 
 too_big() {
-    if [ "$(stat -c %s "$toplevel/$1")" -gt 1000000 ] ; then
+    if [ "$(stat -c %s "$toplevel/$1")" -gt "$maxsize" ] ; then
         cat <<EOF
-Error: Attempting to commit a file larger than approximately 1MB.
+Error: Attempting to commit a file larger than approximately $(maxsize/1000/1000)MB.
 
 Commiting large files slows jenkins builds, clones, and other operations we'd rather not slow down.
 Consider generating, downloading, zipping, etc these files.
@@ -21,5 +21,6 @@ exec 1>&2
 # If you want to allow large files to be committed set this variable to true.
 if ! git config --bool hooks.allowbigfiles ; then
     toplevel=$(git rev-parse --show-toplevel)
+    maxsize=$(git config --int hooks.maxfilesize || echo 1000000)
     git diff --name-only --cached | while read -r x; do too_big "$x"; done
 fi
--- test/tests/maximum-file-size-test.sh
+++ test/tests/maximum-file-size-test.sh
@@ -20,9 +20,13 @@ sh "$hook" > /dev/null 2>&1 && (echo "FAILED: maximum-file-size.sh should exit 1
 # Do nothing if turned off by git configuration.
 git config hooks.allowbigfiles true
 sh "$hook" > /dev/null 2>&1 || (echo "FAILED: maximum-file-size.sh do nothing when hooks.allowbigfiles=true."; exit 1)
-git config hooks.allowbigfiles true
+git config --unset hooks.allowbigfiles
+# Increase max file size above 1MB to allow commit.
+git config hooks.maxfilesize 2000000
+sh "$hook" > /dev/null 2>&1 || (echo "FAILED: maximum-file-size.sh do nothing when hooks.allowbigfiles=true."; exit 1)
+git config --unset hooks.maxfilesize
 # exit 0 when no files larger than 1MB are staged.
 rm file.toobig
 sh "$hook" > /dev/null 2>&1 || (echo "FAILED: maximum-file-size.sh should exit 0 when no >1MB files staged."; exit 1)
 
-echo "PASSED: maximum-file-size.sh exits 1 when >1MB file staged."
+echo "PASSED: maximum-file-size.sh exits 1 when >1MB file staged and 0 otherwise."
