28496f30217041ccea50234339a7135cc018e381
Rudi Kershaw <rudi.kershaw@cognitran.com>
2019-11-12 15:12:38 +0000

    #2 Swap grep -P for perl for x-platform compatability.

--- git-hooks/commit-msg-hooks/msg-matches-regex.sh
+++ git-hooks/commit-msg-hooks/msg-matches-regex.sh
@@ -4,5 +4,5 @@ set -e
 
 if ! git config --bool hooks.allowcommitmsg ; then
     regex=$(git config hooks.commitmsgregex || echo '(\w+\s+){4}')
-    grep -P -q "$regex" "$1" || (echo "Commit message does not adhere to '$regex'"; exit 1)
+    perl -E "exit 1 if <> !~ /$regex/m" "$1"  || (echo "Commit message does not adhere to '$regex'"; exit 1)
 fi
